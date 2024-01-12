8d936b8f1133be24ab4a5cecde5b0d3a849e7008
aklesh yadav <arzunayavadu998@gmail.com>
2024-01-12 12:03:26 +0545

    typing speed cal

--- Python_project/typing_speed_calculator.py
+++ Python_project/typing_speed_calculator.py
@@ -17,22 +17,30 @@ def speed_time(time_s, time_e, userinput):
     time_R = round(time_delay, 2)
     speed = len(userinput) / time_R
     return round(speed)
+if __name__ == '__main__':
+    while True:
+        chk = input("Ready to test : yes / no :")
+        if chk == "yes":
+            test = ["A paragraph is a group of sentences that fleshes out a single idea.",
+                    "My name is Aklesh Yadav", 
+                    "Python is a high-level programming language designed to be easy to read and simple to implement.",
+                    "It is open source, which means it is free to use, evenfor commercial applications."
+                    ]
+            test1 = r.choice(test)
+            title = "*****          Typing Speed Calculator         *****"
+            print(title)
+            print('_'*len(title))
+            print()
+            print(test1)
+            print()
+            time1 = time()
+            testinput = input(" Enter : ")
+            time2 = time()
 
-test = ["A paragraph is a group of sentences that fleshes out a single idea.",
-        "My name is Aklesh Yadav", 
-        "Python is a high-level programming language designed to be easy to read and simple to implement.",
-        "It is open source, which means it is free to use, evenfor commercial applications."
-        ]
-test1 = r.choice(test)
-title = "*****          Typing Speed Calculator         *****"
-print(title)
-print('_'*len(title))
-print()
-print(test1)
-print()
-time1 = time()
-testinput = input(" Enter : ")
-time2 = time()
-
-print("Speed : ", speed_time(time1, time2, testinput), "word/sec")
-print("Error : ", mistake(test1, testinput))
+            print("Speed : ", speed_time(time1, time2, testinput), "word/sec")
+            print("Error : ", mistake(test1, testinput))
+        elif chk == "no":
+            print("Thank You !!!")
+            break
+        else:
+            print('Wrong input.!!!') 
