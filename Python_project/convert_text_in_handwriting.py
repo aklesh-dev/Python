051f0068c469a336f704bf45d60aad2870b7191c
aklesh yadav <arzunayavadu998@gmail.com>
2024-01-30 12:22:12 +0545

    text in handwriting

---
+++ Python_project/convert_text_in_handwriting.py
@@ -0,0 +1,13 @@
+# pywhatkit v-4.6
+import pywhatkit as pw
+
+txt = "PyWhatKit is a Python library with various helpful features.It's easy-to-use and does not require you to do any additional setup."
+
+# try:
+# pw.text_to_handwriting(txt,"demo1.png",rgb=(196, 213, 0))
+print("Program Executed")
+# except:
+    # print("An unknown error occur!!!")
+
+
+
